0cdd083bf96f4a67dc91269ea91c049831e05a2b
cydknee <14293063+cydknee@users.noreply.github.com>
2020-07-20 22:33:31 +0100

    Added protothreading

--- src/app.ino
+++ src/app.ino
@@ -1,4 +1,6 @@
 #include "FastLED.h"
+#include <SoftwareSerial.h>
+#include <protothreads.h>
 
 #define DATA_PIN    3
 #define LED_TYPE    WS2812B
@@ -12,9 +14,17 @@ CRGB targetColour[NUM_LEDS];
 
 CRGB colourPallete[4] = {CRGB::Aqua, CRGB::Blue, CRGB::DarkBlue, CRGB::DodgerBlue};
 
+SoftwareSerial mySerial(6,5); // pins for bluetooth
+
+static struct pt pt1, pt2; // declare protothreads
+
 void setup()
 {
-    Serial.begin(4800); // For debugging
+    PT_INIT(&pt1);
+    PT_INIT(&pt2);
+
+    Serial.begin(9600); //open the serial port for debugging
+    mySerial.begin(9600); // open the bluetooth serial port
 
     FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
     FastLED.setBrightness(BRIGHTNESS);
@@ -22,7 +32,32 @@ void setup()
     setupMood();  
 }
 
+static int readSerialThread(struct pt *pt, int interval) {
+    static unsigned long timestamp = 0;
+    PT_BEGIN(pt);
+        while(1) {
+        PT_WAIT_UNTIL(pt, millis() - timestamp > interval );
+        timestamp = millis(); // take a new timestamp
+        if(mySerial.available()){
+            Serial.println(mySerial.readString()); // send from serial to bluetooth
+        }
+    }
+    PT_END(pt);
+}
+
+static int moodThread(struct pt *pt, int interval) {
+    static unsigned long timestamp = 0;
+    PT_BEGIN(pt);
+    while(1) {
+        PT_WAIT_UNTIL(pt, millis() - timestamp > interval );
+        timestamp = millis();
+        mood(); 
+    }
+    PT_END(pt);
+}
+
 void loop()
 {
-    mood();  
+    readSerialThread(&pt1, 900);
+    moodThread(&pt2, 100);
 }
